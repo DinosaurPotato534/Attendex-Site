80510ed719c5bd999bdc118edb4f608bb5b5f7f4
DinosaurPotato534 <shaany0418@gmail.com>
2023-09-30 18:56:14 -0400

    e
    done editing, this is all finns problem now

--- Attendex/src/components/LandingMorning.tsx
+++ Attendex/src/components/LandingMorning.tsx
@@ -7,6 +7,14 @@ function LandingMorning() {
         <div className="row">
           <div className="text-center">
             <img src="/sunrise.png" className="img-fluid image" />
+            <div className="row">
+              <div className="col">
+                <h1 className="text">Good Morning</h1>
+              </div>
+              <div className="col">
+                <img src="/morning.svg" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
