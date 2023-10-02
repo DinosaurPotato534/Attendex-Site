4e1e025ec5114887b4aefb0a42acd7927f77fb70
IntoTheNubiverse <146592434+IntoTheNubiverse@users.noreply.github.com>
2023-10-02 01:07:09 +0000

    new images and files

---
+++ Attendex/src/components/Link.tsx
@@ -0,0 +1,30 @@
+import "./styles/Link.css";
+
+<div className="container">
+  <div className="widget">
+    <img src="/file Schoology-Logo-WHITE 1"></img>
+
+
+
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+export default Link;
--- Attendex/src/main.tsx
+++ Attendex/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
+
+
 import MainSection from "./components/sections/MainSection";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
